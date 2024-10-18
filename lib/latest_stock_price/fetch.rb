9f64dd9fe3e1921a3f7643200013a83831c23987
wafihfzhn <hafizhuddin96@gmail.com>
2024-10-18 23:41:48 +0800

    Add fetch data of equities

--- lib/latest_stock_price/fetch.rb
+++ lib/latest_stock_price/fetch.rb
@@ -1,6 +1,11 @@
 module LatestStockPrice
   class Fetch < Connection
     class << self
+      def equities
+        path = "/equities"
+        send_request("get", path)
+      end
+
       def price_all
         path = "/any"
         send_request("get", path)
